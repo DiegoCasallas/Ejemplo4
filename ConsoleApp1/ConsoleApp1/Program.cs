419092115b188601b5ad1c1cdeb2a3f9f05d4113
Rentadvisor <Rentadvisor@AV-26366>
2019-08-31 23:00:37 -0500

    cambios2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola1");
+            Console.WriteLine("Hola13");
         }
     }
 }
